8d41bfa6c038fc4dd6bacf66b617dc2c861b1277
Jorge Saa <piojo.sistematico@gmail.com>
2023-11-23 20:53:43 -0300

    refactored the data and added new comment functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import Comment from "./components/Comment";
 import { Button, TextArea, TextField } from "react-aria-components";
+import CommentComponent from "./components/Comment";
 
 type User = {
   image: {
@@ -16,73 +16,105 @@ type Comment = {
   createdAt: string;
   score: number;
   user: User;
-  replies: Comment[];
-};
-
-type Data = {
-  currentUser: User;
-  comments: Comment[];
+  replies: number[];
 };
 
 function App() {
-  const [data, setData] = useState<Data>({
-    currentUser: {
-      image: {
-        png: "string",
-        webp: "string",
-      },
-      username: "string",
+  const [thread, setThread] = useState([1, 2]);
+
+  const [currentUser, setCurrentUser] = useState<User>({
+    image: {
+      png: "string",
+      webp: "string",
     },
-    comments: [
-      {
-        id: 1,
-        content: "string",
-        createdAt: "string",
-        score: 1,
-        user: {
-          image: {
-            png: "string",
-            webp: "string",
-          },
-          username: "string",
-        },
-        replies: [],
-      },
-    ],
+    username: "string",
   });
 
+  const [comments, setComments] = useState<Comment[]>([
+    {
+      id: 1,
+      content: "string",
+      createdAt: "string",
+      score: 1,
+      user: {
+        image: {
+          png: "string",
+          webp: "string",
+        },
+        username: "string",
+      },
+      replies: [],
+    },
+  ]);
+
   useEffect(() => {
-    fetch("data.json")
+    fetch("currentUser.json")
       .then((res) => res.json())
-      .then((info) => setData(info));
+      .then((info) => setCurrentUser(info));
+    fetch("comments.json")
+      .then((res) => res.json())
+      .then((info) => setComments(info));
   }, []);
 
-  console.log(data);
+  console.log("user>>>", currentUser);
+  console.log("comments>>>", comments);
+  console.log(
+    "filter>>>",
+    comments.filter((elem) => thread.includes(elem.id)),
+  );
+
+  function addNewComment(e): void {
+    e.preventDefault();
+    console.log(e);
+    setComments([
+      ...comments,
+      {
+        id: 1,
+        content: e.target.content.value,
+        createdAt: "1 min ago",
+        score: 1,
+        user: currentUser,
+        replies: [],
+      },
+    ]);
+  }
 
   return (
     <>
-      <main className="bg-neutral-4 font-rubik text-neutral-2 flex flex-col gap-4 p-8 text-base font-normal">
-        {data.comments.map((elem, index) => (
-          <Comment key={index} {...elem} {...data.currentUser}></Comment>
-        ))}
+      <main className="flex flex-col gap-4 bg-neutral-4 p-8 font-rubik text-base font-normal text-neutral-2">
+        {comments
+          .filter((elem) => thread.includes(elem.id))
+          .map((elem, index) => (
+            <CommentComponent
+              key={index}
+              id={elem.id}
+              content={elem.content}
+              createdAt={elem.createdAt}
+              score={elem.score}
+              user={elem.user}
+              replies={elem.replies}
+              currentUser={currentUser}
+              comments={comments}
+            ></CommentComponent>
+          ))}
         <form
+          onSubmit={addNewComment}
           action=""
-          className="bg-neutral-5 flex flex-col gap-4 rounded-md p-4"
+          className="flex flex-col gap-4 rounded-md bg-neutral-5 p-4"
         >
-          <TextField className="w-full">
-            <TextArea
-              defaultValue="Add a comment..."
-              className="border-neutral-3 w-full rounded-md border-2 border-solid p-2"
-            ></TextArea>
-          </TextField>
+          <textarea
+            placeholder="Add a comment..."
+            name="content"
+            className="w-full rounded-md border-2 border-solid border-neutral-3 p-2"
+          ></textarea>
           <div className="flex flex-row items-center justify-between">
             <picture className="h-8 w-8">
-              <img
-                src={`src/assets/${data.currentUser.image.png}`}
-                alt="Avatar"
-              />
+              <img src={`src/assets/${currentUser.image.png}`} alt="Avatar" />
             </picture>
-            <Button className="bg-primary-1 text-neutral-5 rounded-md px-6 py-2 uppercase">
+            <Button
+              type="submit"
+              className="rounded-md bg-primary-1 px-6 py-2 uppercase text-neutral-5"
+            >
               Send
             </Button>
           </div>
--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,5 +1,6 @@
 import { Button } from "react-aria-components";
 import { IconDelete, IconEdit, IconMinus, IconPlus, IconReply } from "./Icons";
+import { useState } from "react";
 
 type User = {
   image: {
@@ -15,29 +16,46 @@ type CommentProp = {
   createdAt: string;
   score: number;
   user: User;
-  replies?: CommentProp[];
+  replies: number[];
+  comments: Comment[];
+  currentUser: User;
 };
 
-const Comment: React.FunctionComponent<CommentProp & User> = ({
+type Comment = {
+  id: number;
+  content: string;
+  createdAt: string;
+  score: number;
+  user: User;
+  replies: number[];
+};
+
+const CommentComponent: React.FunctionComponent<CommentProp> = ({
   id,
   content,
   createdAt,
   score,
   user,
   replies,
-  image,
-  username,
+  comments,
+  currentUser,
 }) => {
+  const [isReplyOpen, setIsReplyOpen] = useState(false);
+
+  function handleReply(): void {
+    setIsReplyOpen(!isReplyOpen);
+  }
+
   return (
     <>
-      <article className="bg-neutral-5 flex flex-col gap-4 rounded-md p-4">
+      <article className="flex flex-col gap-4 rounded-md bg-neutral-5 p-4">
         <div className="flex flex-row items-center gap-4">
           <picture className="h-8 w-8">
             <img src={`src/assets/${user.image.png}`} alt="Avatar" />
           </picture>
-          <span className="text-neutral-1 font-medium">{user.username}</span>
-          {user.username == username ? (
-            <span className="bg-primary-1 text-neutral-5 px-1 text-sm">
+          <span className="font-medium text-neutral-1">{user.username}</span>
+          {user.username == currentUser.username ? (
+            <span className="bg-primary-1 px-1 text-sm text-neutral-5">
               You
             </span>
           ) : null}
@@ -45,29 +63,32 @@ const Comment: React.FunctionComponent<CommentProp & User> = ({
         </div>
         <p>{content}</p>
         <div className="flex flex-row items-center justify-between">
-          <div className="bg-neutral-4 flex flex-row items-center gap-4 rounded-md p-2">
+          <div className="flex flex-row items-center gap-4 rounded-md bg-neutral-4 p-2">
             <Button>
               <IconPlus></IconPlus>
             </Button>
-            <span className="text-primary-1 font-medium">{score}</span>
+            <span className="font-medium text-primary-1">{score}</span>
             <Button>
               <IconMinus></IconMinus>
             </Button>
           </div>
           <div className="flex flex-row items-center gap-4">
-            {user.username == username ? (
+            {user.username == currentUser.username ? (
               <>
-                <Button className="text-primary-2 flex flex-row items-center gap-2 font-medium">
+                <Button className="flex flex-row items-center gap-2 font-medium text-primary-2">
                   <IconDelete></IconDelete>
                   <span>Delete</span>
                 </Button>
-                <Button className="text-primary-1 flex flex-row items-center gap-2 font-medium">
+                <Button className="flex flex-row items-center gap-2 font-medium text-primary-1">
                   <IconEdit></IconEdit>
                   <span>Edit</span>
                 </Button>
               </>
             ) : (
-              <Button className="text-primary-1 flex flex-row items-center gap-2 font-medium">
+              <Button
+                onPress={handleReply}
+                className="flex flex-row items-center gap-2 font-medium text-primary-1"
+              >
                 <IconReply></IconReply>
                 <span>Reply</span>
               </Button>
@@ -77,19 +98,45 @@ const Comment: React.FunctionComponent<CommentProp & User> = ({
       </article>
 
       {replies && replies.length > 0 ? (
-        <div className="border-neutral-3 flex flex-col gap-4 border-l-2 border-solid pl-4">
-          {replies.map((elem, index) => (
-            <Comment
-              key={index}
-              {...elem}
-              image={image}
-              username={username}
-            ></Comment>
-          ))}
+        <div className="flex flex-col gap-4 border-l-2 border-solid border-neutral-3 pl-4">
+          {comments
+            .filter((elem) => replies.includes(elem.id))
+            .map((elem, index) => (
+              <CommentComponent
+                key={index}
+                id={elem.id}
+                content={elem.content}
+                createdAt={elem.createdAt}
+                score={elem.score}
+                user={elem.user}
+                replies={elem.replies}
+                currentUser={currentUser}
+                comments={comments}
+              ></CommentComponent>
+            ))}
         </div>
+      ) : null}
+
+      {isReplyOpen ? (
+        <form
+          action=""
+          className="flex flex-row items-start gap-4 rounded-md bg-neutral-5 p-4"
+        >
+          <picture className="h-12 w-12">
+            <img src={`src/assets/${currentUser.image.png}`} alt="Avatar" />
+          </picture>
+          <textarea
+            name=""
+            id=""
+            className="w-full rounded-md border-2 border-solid border-neutral-3 p-2"
+          ></textarea>
+          <Button className="rounded-md bg-primary-1 px-4 py-1 uppercase text-neutral-5">
+            Reply
+          </Button>
+        </form>
       ) : null}
     </>
   );
 };
 
-export default Comment;
+export default CommentComponent;
